73d2ed08e1bd7847db2cdb837f19b7eba4312084
moez <moezaloui123@gmail.com>
2024-02-04 00:27:55 +0100

    fix: problems

--- frontend/ecom/src/Components/Menu/MenuBar.jsx
+++ frontend/ecom/src/Components/Menu/MenuBar.jsx
@@ -6,12 +6,43 @@ function MenuBar() {
     <>
     <div className="container-menu h-[100vh]">
       <ul className="menuBar">
-        <li>memu 1</li>
-        <li>memu 2</li>
-        <li>memu 3</li>
-        <li>memu 4</li>
-        <li>memu 5</li>
-      </ul>
+ 
+  <li>
+    memu 1
+    <ul class="submenu">
+      <li>Sous-menu 1.1</li>
+      <li>Sous-menu 1.2</li>
+    </ul>
+  </li>
+  <li>
+    memu 2
+    <ul class="submenu">
+      <li>Sous-menu 2.1</li>
+      <li>Sous-menu 2.2</li>
+    </ul>
+  </li>
+  <li>
+    memu 3
+    <ul class="submenu">
+      <li>Sous-menu 3.1</li>
+      <li>Sous-menu 3.2</li>
+    </ul>
+  </li>
+  <li>
+    memu 4
+    <ul class="submenu">
+      <li>Sous-menu 4.1</li>
+      <li>Sous-menu 4.2</li>
+    </ul>
+  </li>
+  <li>
+    memu 5
+    <ul class="submenu">
+      <li>Sous-menu 5.1</li>
+      <li>Sous-menu 5.2</li>
+    </ul>
+  </li>
+</ul>
     </div>
     <div className="center-screen">
       <div className="landing-page-container">
