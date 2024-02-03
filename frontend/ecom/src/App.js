28184351505aa61e8e0f2a8289a58153f0b5b149
moez <moezaloui123@gmail.com>
2024-02-03 21:59:47 +0100

    feat: add menu bar component

--- frontend/ecom/src/App.js
+++ frontend/ecom/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import 'animate.css';
+import './Components/Menu/menuBar.css';
+import MenuBar from './Components/Menu/MenuBar';
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <MenuBar></MenuBar>
+    </div>
   )
 }
---
+++ frontend/ecom/src/Components/Menu/MenuBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function MenuBar() {
+  return (
+    <>
+    <div className="container-menu">
+      <ul className="menuBar">
+        <li>memu 1</li>
+        <li>memu 2</li>
+        <li>memu 3</li>
+        <li>memu 4</li>
+        <li>memu 5</li>
+      </ul>
+    </div>
+    <div className="center-screen">
+      <div className="landing-page-container">
+        <div className="container-message">
+            <p className="text1">Buy Your Phone Now</p>
+            <p className="text2">Next-Level Mobile Innovation </p>
+            <p className="text3">Shop Now</p>
+            <button className="button-book"> <span>Book Now !</span></button>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default MenuBar
