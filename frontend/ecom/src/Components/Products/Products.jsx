ad03b8d4bdf6cc18125eed49a50bbeb11ac136b7
moez <moezaloui123@gmail.com>
2024-02-04 03:16:52 +0100

    faet: add card product

--- frontend/ecom/src/Components/Products/Products.jsx
+++ frontend/ecom/src/Components/Products/Products.jsx
@@ -10,24 +10,40 @@ function Products() {
             <p>Top Reated</p>
         </div>
 <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-<li class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow" style={{ height: '20rem', cursor: 'pointer' }}>
+ <li class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow" style={{ height: '27rem', cursor: 'pointer' }}>
+    <img srcset={a} class="w-full h-auto max-w-xl rounded-lg" alt="image description" className="pic-card" />   
     <div class="flex w-full items-center justify-between space-x-6 p-6">
-        <img srcset={a} class="w-full h-auto max-w-xl rounded-lg" alt="image description" className="pic-card" />
         <div class="hover-content flex-1 truncate">
-            <div class="flex items-center space-x-3">
-                <h3 class="truncate text-sm font-medium text-gray-900" style={{ marginTop: '10rem' }}>Iphone 11</h3>
-            </div>
-            <p class="mt-1 truncate text-sm text-gray-500">
+            <div>
+            <h3 class="truncate text-sm font-medium text-gray-900" sx={{position:'absolute', marginTop:'24%', marginBottom:'3%'}}>Iphone 11</h3>
+            <p class="mt-1 truncate text-sm text-gray-500"  sx={{marginTop:'-4%'}}>
                 Color: gray, gold, blue.<br />
                 Battery: 45500mAh.<br />
                 Stock: in stock.<br />
                 Price: $300.
             </p>
         </div>
+        </div>
     </div>
-</li>
 
- 
+</li>
+<li class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow" style={{ height: '27rem', cursor: 'pointer' }}>
+    <img srcset={a} class="w-full h-auto max-w-xl rounded-lg" alt="image description" className="pic-card" />   
+    <div class="flex w-full items-center justify-between space-x-6 p-6">
+        <div class="hover-content flex-1 truncate">
+            <div>
+            <h3 class="truncate text-sm font-medium text-gray-900" sx={{position:'absolute', marginTop:'24%', marginBottom:'3%'}}>Iphone 11</h3>
+            <p class="mt-1 truncate text-sm text-gray-500"  sx={{marginTop:'-4%'}}>
+                Color: gray, gold, blue.<br />
+                Battery: 45500mAh.<br />
+                Stock: in stock.<br />
+                Price: $300.
+            </p>
+        </div>
+        </div>
+    </div>
+
+</li>
 </ul>
 
 
