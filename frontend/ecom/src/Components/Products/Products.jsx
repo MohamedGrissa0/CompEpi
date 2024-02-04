454d018b742596f09afd692f8f17a94e1bab7d2a
moez <moezaloui123@gmail.com>
2024-02-04 03:27:46 +0100

    fix card

--- frontend/ecom/src/Components/Products/Products.jsx
+++ frontend/ecom/src/Components/Products/Products.jsx
@@ -28,6 +28,9 @@ function Products() {
 
 </li>
 <li class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow" style={{ height: '27rem', cursor: 'pointer' }}>
+<svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.3L19 7H7.3"/>
+  </svg>
     <img srcset={a} class="w-full h-auto max-w-xl rounded-lg" alt="image description" className="pic-card" />   
     <div class="flex w-full items-center justify-between space-x-6 p-6">
         <div class="hover-content flex-1 truncate">
