e4bae4fac63580b67ddf964ca64d23208778e1aa
Med Grissa <mouhamedgrissa0@gmail.com>
2024-02-04 03:26:52 +0100

    -i-

--- frontend/ecom/src/App.js
+++ frontend/ecom/src/App.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from 'react';
 import 'animate.css';
 import Navbar from './Components/Navbar/Navbar';
 import './Components/Menu/menuBar.css';
 import MenuBar from './Components/Menu/MenuBar';
 import Slider from './Components/Slider/Slider';
+import { Slide } from 'react-toastify';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Page from './Components/Page/Page';
 
 export default function App() {
   return (
-    <div className='h-full'>
-      <Navbar />
-      <MenuBar />
-        <Slider/>
-      </div>
-  )
+  
+            <div>
+              <Navbar/>
+              <MenuBar />
+              <Slider />
+              
+              
+              
+              
+            </div>
+         
+  );
 }
--- frontend/ecom/src/Components/Menu/MenuBar.jsx
+++ frontend/ecom/src/Components/Menu/MenuBar.jsx
@@ -1,62 +1,19 @@
-import React from 'react'
-import Phone from '../../assets/phone-pic/phone.png'
+import React from 'react';
+import Phone from '../../assets/phone-pic/phone.png';
+import big from "../../assets/a.jpg";
 
 function MenuBar() {
+  const containerStyle = {
+    backgroundImage: `url(${big})`,
+  };
+
   return (
-    <>
-    <div className="container-menu h-[100vh]">
-      <ul className="menuBar">
- 
-  <li>
-    memu 1
-    <ul class="submenu">
-      <li>Sous-menu 1.1</li>
-      <li>Sous-menu 1.2</li>
-    </ul>
-  </li>
-  <li>
-    memu 2
-    <ul class="submenu">
-      <li>Sous-menu 2.1</li>
-      <li>Sous-menu 2.2</li>
-    </ul>
-  </li>
-  <li>
-    memu 3
-    <ul class="submenu">
-      <li>Sous-menu 3.1</li>
-      <li>Sous-menu 3.2</li>
-    </ul>
-  </li>
-  <li>
-    memu 4
-    <ul class="submenu">
-      <li>Sous-menu 4.1</li>
-      <li>Sous-menu 4.2</li>
-    </ul>
-  </li>
-  <li>
-    memu 5
-    <ul class="submenu">
-      <li>Sous-menu 5.1</li>
-      <li>Sous-menu 5.2</li>
-    </ul>
-  </li>
-</ul>
-    </div>
-    <div className="center-screen">
-      <div className="landing-page-container">
-        <div className="container-message">
-            <p className="text1">Buy Your Phone Now</p>
-            <p className="text2">Next-Level Mobile Innovation </p>
-            <p className="text3">Shop Now</p>
-            <button className="button-book"> <span>Book Now !</span></button>
-        </div>
+    <div className='h-[90vh] bg-cover bg-center' style={containerStyle}>
+      <div className="container mx-auto">
+        {/* Your content goes here */}
       </div>
-      <img src={Phone} alt='' className="landing-pic"></img> 
     </div>
-    </>
-  )
+  );
 }
 
-export default MenuBar
+export default MenuBar;
